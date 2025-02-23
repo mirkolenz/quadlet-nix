8cd9b2ee7fd37443b22968f4d39174a6f8ee5ba9
Mirko Lenz <mirko@mirkolenz.com>
2025-02-23 17:53:46 +0100

    fix(network): delete on stop

--- quadlet-modules/network.nix
+++ quadlet-modules/network.nix
@@ -1,6 +1,8 @@
 {
   config,
   lib',
+  podman,
+  lib,
   ...
 }:
 {
@@ -14,6 +16,9 @@
     serviceName = "${config.name}-network";
     podmanName = config.networkConfig.NetworkName or "systemd-${config.name}";
     unitConfig.Description = "Podman network ${config.name}";
+    serviceConfig = {
+      ExecStop = "${lib.getExe podman} network rm ${config.podmanName}";
+    };
 
     finalConfig.Network = config.networkConfig;
   };
