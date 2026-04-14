069ae8c3564caed25d4898ee744cddf838c11300
Mirko Lenz <mirko@mirkolenz.com>
2026-04-14 09:13:27 +0200

    chore(deps/nix): update
    Flake lock file updates:
    
    • Updated input 'home-manager':
        'github:nix-community/home-manager/d21bee5' (2026-01-16)
      → 'github:nix-community/home-manager/8a423e4' (2026-04-14)

--- tests/flake.nix
+++ tests/flake.nix
@@ -2,10 +2,10 @@
   description = "Development inputs for quadlet-nix tests. These do not appear in consumers' lock files.";
   inputs = {
     # quadlet-nix.url = "..";
-    # nixpkgs.follows = "quadlet-nix/nixpkgs";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "";
+      # inputs.nixpkgs.follows = "quadlet-nix/nixpkgs";
     };
   };
   outputs = _: { };
