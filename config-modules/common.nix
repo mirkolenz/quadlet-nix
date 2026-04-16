31150f140ad1d4f43555f4b5ae1234e98b042180
Mirko Lenz <mirko@mirkolenz.com>
2026-04-16 15:36:18 +0200

    fix(common): add artifacts to quadlet objects

--- config-modules/common.nix
+++ config-modules/common.nix
@@ -5,13 +5,14 @@ let
 
   duplicateNames = lib.intersectLists (lib.attrNames cfg.containers) (lib.attrNames cfg.pods);
   concatObjects = lib.concatMap lib.attrValues [
+    cfg.artifacts
+    cfg.builds
     cfg.containers
+    cfg.images
+    cfg.kubes
     cfg.networks
     cfg.pods
-    cfg.kubes
     cfg.volumes
-    cfg.builds
-    cfg.images
   ];
 in
 {
@@ -34,20 +35,19 @@ in
     };
   };
   config = lib.mkIf (cfg.enable && cfg.allObjects != [ ]) {
-    assertions =
-      [
-        {
-          assertion = duplicateNames == [ ];
-          message = ''
-            The container/pod names should be unique!
-            See: https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html#podname
-            The following names are not unique: ${lib.concatStringsSep " " duplicateNames}
-          '';
-        }
-      ]
-      ++ (map (obj: {
-        assertion = obj.imageFile == null || obj.imageStream == null;
-        message = "Only one of imageFile and imageStream can be set for container ${obj.name}";
-      }) (lib.attrValues cfg.containers));
+    assertions = [
+      {
+        assertion = duplicateNames == [ ];
+        message = ''
+          The container/pod names should be unique!
+          See: https://docs.podman.io/en/latest/markdown/podman-systemd.unit.5.html#podname
+          The following names are not unique: ${lib.concatStringsSep " " duplicateNames}
+        '';
+      }
+    ]
+    ++ (map (obj: {
+      assertion = obj.imageFile == null || obj.imageStream == null;
+      message = "Only one of imageFile and imageStream can be set for container ${obj.name}";
+    }) (lib.attrValues cfg.containers));
   };
 }
